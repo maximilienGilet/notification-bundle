51c51201a05e34b2d4c4865c5634022bf726c75c
Maximilien Gilet <maximilien.gilet@hotmail.fr>
2017-11-22 23:02:24 +0100

    Add ReadTheDocs config

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to mgilet/notification-bundle\u2019s documentation!"],titleterms:{bundl:0,document:0,indic:0,mgilet:0,notif:0,tabl:0,welcom:0}})
