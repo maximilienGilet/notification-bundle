e7f2e7a0968a7459c99b3d3d83e118ea223d0cff
David Sanchez <david.sanchez@nventive.com>
2018-07-27 09:31:08 -0400

    Add new entity class `Notification`

---
+++ Entity/Notification.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Mgilet\NotificationBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Mgilet\NotificationBundle\Model\Notification as NotificationModel;
+
+/**
+ * Class Notification
+ *
+ * @ORM\Entity
+ * @package Mgilet\NotificationBundle\Entity
+ */
+class Notification extends NotificationModel
+{
+
+}
